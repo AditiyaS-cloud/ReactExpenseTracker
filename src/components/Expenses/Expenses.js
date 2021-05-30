df88a7b9208c0e94dc5590aec39fb7f6f0093313
AditiyaS-cloud <71305712+AditiyaS-cloud@users.noreply.github.com>
2021-05-30 13:38:28 +0530

    Changed expense list to dynamic behaviour

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,5 +1,4 @@
 import ExpenseItem from './ExpenseItem';
-
 import Card from '../UI/Card';
 import './Expenses.css';
 import ExpensesFilter from './ExpenseFilter';
@@ -15,26 +14,11 @@ const Expenses = (props) => {
         <div>
             <Card className="expenses">
                 <ExpensesFilter selected={filteredYear} onSelectFilterData={onSelectYearHandler} />
-                <ExpenseItem
-                    title={props.items[0].title}
-                    amount={props.items[0].amount}
-                    date={props.items[0].date} />
-                <ExpenseItem
-                    title={props.items[1].title}
-                    amount={props.items[1].amount}
-                    date={props.items[1].date} />
-                <ExpenseItem
-                    title={props.items[2].title}
-                    amount={props.items[2].amount}
-                    date={props.items[2].date} />
-                <ExpenseItem
-                    title={props.items[3].title}
-                    amount={props.items[3].amount}
-                    date={props.items[3].date} />
+                {props.items.map((expense) => (
+                    <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date} />
+                ))}
             </Card>
         </div>
-
-
     )
 }
 
